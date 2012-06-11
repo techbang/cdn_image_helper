1f01f33fbce8078c90aeeff132591a0ec03094f3
Chun-wei Kuo <Dendoh@gmail.com>
2012-06-11 16:35:45 +0800

    The same path should have the same cdn host

--- lib/cdn_image_helper.rb
+++ lib/cdn_image_helper.rb
@@ -13,9 +13,9 @@ module CdnImageHelper
     escaped_app_url = Regexp.escape(CdnImageHelper.app_url)
     text.gsub(/(href|src)="(#{escaped_app_url}(\.tw)?)?\/system\/images\/(\d+)\/(\w+)\//) do |match|
       link_type, host, tw, id, scale = $1, $2, $3, $4, $5
-      cdn_host = CdnImageHelper.cdn_host % rand(4) if Rails.env.production?
-      new_size = link_type == "href" ? :original : size
-      %|#{link_type}="#{cdn_host}/system/images/#{id}/#{new_size}/|
+      new_size = (link_type == "href") ? :original : size
+      path = "/system/images/#{id}/#{new_size}/"
+      %|#{link_type}="#{Rails.env.production? ? cdn_url_for(path) : path}|
     end
   end
 
@@ -25,8 +25,8 @@ module CdnImageHelper
     escaped_app_url = Regexp.escape(CdnImageHelper.app_url)
     text.gsub(/(#{escaped_app_url}(\.tw)?)?\/system\/excerpt_images\/(\d+)\/(\w+)\//) do |match|
       host, tw, id, scale = $1, $2, $3, $4
-      cdn_host = CdnImageHelper.cdn_host % rand(4) if Rails.env.production?
-      %|#{cdn_host}/system/excerpt_images/#{id}/#{size}/|
+      path = "/system/excerpt_images/#{id}/#{size}/"
+      Rails.env.production? ? cdn_url_for(path) : path
     end
   end
 
@@ -37,6 +37,12 @@ module CdnImageHelper
       id, scale = $1, $2
       %|/system/images/#{id}/#{size}/|
     end
+  end
+
+  def cdn_url_for(path, cdn_host = CdnImageHelper.cdn_host)
+    cdn_number = Digest::MD5.hexdigest(path).to_i(16) % 4
+    cdn_host = cdn_host % cdn_number
+    "#{cdn_host}#{path}"
   end
   
 end
