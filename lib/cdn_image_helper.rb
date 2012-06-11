7b97775ce80691754b8f19f992bc8cc4c7c87688
Chun-wei Kuo <Dendoh@gmail.com>
2012-06-11 16:28:00 +0800

    Match *.tw and escape dots

--- lib/cdn_image_helper.rb
+++ lib/cdn_image_helper.rb
@@ -9,8 +9,10 @@ module CdnImageHelper
   
   def render_with_cdn_images(text, size = :medium)
     return text unless text.respond_to?(:gsub)
-    text.gsub(/(href|src)="(#{CdnImageHelper.app_url})?\/system\/images\/(\d+)\/(\w+)\//) do |match|
-      link_type, host, id, scale = $1, $2, $3, $4
+
+    escaped_app_url = Regexp.escape(CdnImageHelper.app_url)
+    text.gsub(/(href|src)="(#{escaped_app_url}(\.tw)?)?\/system\/images\/(\d+)\/(\w+)\//) do |match|
+      link_type, host, tw, id, scale = $1, $2, $3, $4, $5
       cdn_host = CdnImageHelper.cdn_host % rand(4) if Rails.env.production?
       new_size = link_type == "href" ? :original : size
       %|#{link_type}="#{cdn_host}/system/images/#{id}/#{new_size}/|
@@ -19,8 +21,10 @@ module CdnImageHelper
 
   def render_with_cdn_excerpt_images(text, size = :medium)
     return text unless text.respond_to?(:gsub)
-    text.gsub(/(#{CdnImageHelper.app_url})?\/system\/excerpt_images\/(\d+)\/(\w+)\//) do |match|
-      host, id, scale = $1, $2, $3
+
+    escaped_app_url = Regexp.escape(CdnImageHelper.app_url)
+    text.gsub(/(#{escaped_app_url}(\.tw)?)?\/system\/excerpt_images\/(\d+)\/(\w+)\//) do |match|
+      host, tw, id, scale = $1, $2, $3, $4
       cdn_host = CdnImageHelper.cdn_host % rand(4) if Rails.env.production?
       %|#{cdn_host}/system/excerpt_images/#{id}/#{size}/|
     end
@@ -28,6 +32,7 @@ module CdnImageHelper
 
   def replace_image_size(text, size = :medium)
     return text unless text.respond_to?(:gsub)
+
     text.gsub(/\/system\/images\/(\d+)\/(\w+)\//) do |match|
       id, scale = $1, $2
       %|/system/images/#{id}/#{size}/|
